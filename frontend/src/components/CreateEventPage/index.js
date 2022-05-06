f2d3eabe42df64d9abaffaef10cb2992dd6bf1b2
Agustin Zucca <zucca.agustin98@gmail.com>
2022-05-06 15:29:50 -0500

    more work

--- frontend/src/components/CreateEventPage/index.js
+++ frontend/src/components/CreateEventPage/index.js
@@ -5,7 +5,7 @@ import { Redirect, useHistory } from "react-router-dom";
 import "./CreateEvent.css";
 
 const CreateEventFormPage = () => {
-  const history = useHistory()
+  const history = useHistory();
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
   const categories = useSelector((state) => state.events.categories);
@@ -50,118 +50,123 @@ const CreateEventFormPage = () => {
     );
 
     if (event) {
-      history.push('/events')
+      history.push("/events");
       return;
     }
   };
 
   return (
     <div className="createEventPage">
-      <form className="createEventForm" onSubmit={handleSubmit}>
-        <div className="basicInfo">
-          <h1>Basic Info</h1>
-          <p>
-            Name your event and tell event-goers why they should come. Add
-            details that highlight what makes it unique.
-          </p>
-        </div>
-        <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
-        <div className="eventNameInput formInput">
-          <label className="eventcreatelabel">Event Title</label>
-          <input
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-            required
-            placeholder="Be descriptive and clear"
-          ></input>
-        </div>
-        <div className="eventDescriptionInput formInput">
-          <label className="eventcreatelabel">Event description</label>
-          <input
-            onChange={(e) => setDescription(e.target.value)}
-            value={description}
-            required
-            placeholder="Tell people what your event is all about!"
-          ></input>
-        </div>
-        <div className="categorySelection formInput">
-          <label className="eventcreatelabel">Category</label>
-          <select
-            onChange={(e) => setCategory(e.target.value)}
-            value={category}
-          >
-            <option defaultValue hidden>
-              Category
-            </option>
-            {categories.map(({ id, name }) => (
-              <option key={id} value={id}>
-                {name}
-              </option>
+      <div className="createEventForm">
+        <form className="createEventForm" onSubmit={handleSubmit}>
+          <div className="basicInfo">
+            <h1>Basic Info</h1>
+            <p>
+              Name your event and tell event-goers why they should come. Add
+              details that highlight what makes it unique.
+            </p>
+          </div>
+          <ul>
+            {errors.map((error, idx) => (
+              <li key={idx}>{error}</li>
             ))}
-          </select>
-        </div>
-        <div className="eventDescriptionInput formInput">
-          <label className="eventcreatelabel">Event Image</label>
-          <input
-            onChange={(e) => setImg(e.target.value)}
-            value={img}
-            required
-            placeholder="Enter your event image url here"
-          ></input>
-        </div>
-        <div className="locationInfo">
-          <h1>Location</h1>
-          <p>
-            Help people in the area discover your event and let attendees know
-            where to show up.
-          </p>
-        </div>
-        <div className="locationInput formInput">
-          <label className="eventcreatelabel">Location</label>
-          <input
-            type="address"
-            onChange={(e) => setLocation(e.target.value)}
-            value={location}
-            required
-          ></input>
-        </div>
-        <div className="dateInfo">
-          <h1>Date</h1>
-          <p>
-          Tell event-goers when your event starts so they can make plans to attend.
-          </p>
-        </div>
-        <div className="formInput">
-          <label className="eventcreatelabel">Date</label>
-          <input
-            type="datetime-local"
-            onChange={(e) => setDate(e.target.value)}
-            value={date}
-            required
-            className="dateInput"
-          ></input>
-        </div>
-        <div className="capacityInfo">
-          <h1>Capacity</h1>
-          <p>
-          How many tickets will be available for this event.
-          </p>
-        </div>
-        <div className="formInput">
-          <label className="eventcreatelabel">Capacity</label>
-          <input
-            type="numeric"
-            onChange={(e) => setCapacity(e.target.value)}
-            value={capacity}
-            required
-          ></input>
-        </div>
-        <button type="submit">Create Event</button>
-      </form>
+          </ul>
+          <div className="eventNameInput formInput">
+            <label className="eventcreatelabel">Event Title</label>
+            <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              required
+              placeholder="Be descriptive and clear"
+            ></input>
+          </div>
+          <div className="eventDescriptionInput formInput">
+            <label className="eventcreatelabel">Event description</label>
+            <textarea
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
+              required
+              placeholder="Tell people what your event is all about!"
+            ></textarea>
+          </div>
+          <div className="categorySelection formInput">
+            <label className="eventcreatelabel">Category</label>
+            <select
+              onChange={(e) => setCategory(e.target.value)}
+              value={category}
+            >
+              <option defaultValue hidden>
+                Category
+              </option>
+              {categories.map(({ id, name }) => (
+                <option key={id} value={id}>
+                  {name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="eventDescriptionInput formInput">
+            <label className="eventcreatelabel">Event Image</label>
+            <input
+              onChange={(e) => setImg(e.target.value)}
+              value={img}
+              required
+              placeholder="Enter your event image url here"
+            ></input>
+          </div>
+          <div className="locationInfo">
+            <h1>Location</h1>
+            <p>
+              Help people in the area discover your event and let attendees know
+              where to show up.
+            </p>
+          </div>
+          <div className="locationInput formInput">
+            <label className="eventcreatelabel">Location</label>
+            <input
+              type="address"
+              onChange={(e) => setLocation(e.target.value)}
+              value={location}
+              required
+            ></input>
+          </div>
+          <div className="dateInfo">
+            <h1>Date</h1>
+            <p>
+              Tell event-goers when your event starts so they can make plans to
+              attend.
+            </p>
+          </div>
+          <div className="formInput">
+            <label className="eventcreatelabel">Date</label>
+            <input
+              type="datetime-local"
+              onChange={(e) => setDate(e.target.value)}
+              value={date}
+              required
+              className="dateInput"
+            ></input>
+          </div>
+          <div className="capacityInfo">
+            <h1>Capacity</h1>
+            <p>How many tickets will be available for this event.</p>
+          </div>
+          <div className="formInput">
+            <label className="eventcreatelabel">Capacity</label>
+            <input
+              type="numeric"
+              onChange={(e) => setCapacity(e.target.value)}
+              value={capacity}
+              required
+            ></input>
+          </div>
+          <div className="submit">
+            <button type="submit" className="createBtn">
+              Create Event
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- frontend/src/components/EditEventPage/index.js
+++ frontend/src/components/EditEventPage/index.js
@@ -11,19 +11,24 @@ const EditEventPage = () => {
   const categories = useSelector((state) => state.events.categories);
   const events = useSelector((state) => state.events.events);
   const singleEvent = events.find((event) => event.id === parseInt(eventId));
-  const singleCategory = categories.find(category => category?.id === singleEvent?.categoryId)
+  const singleCategory = categories.find(
+    (category) => category?.id === singleEvent?.categoryId
+  );
   const [name, setName] = useState(singleEvent?.name);
   const [date, setDate] = useState(singleEvent?.date);
   const [description, setDescription] = useState(singleEvent?.description);
   const [category, setCategory] = useState(singleCategory?.id);
+  const [img, setImg] = useState(singleEvent?.img);
   const [location, setLocation] = useState(singleEvent?.location);
   const [capacity, setCapacity] = useState(singleEvent?.capacity);
-  const [isLoaded, setIsLoaded] = useState(false)
+  const [isLoaded, setIsLoaded] = useState(false);
   const [errors, setErrors] = useState([]);
 
   useEffect(() => {
-    dispatch(eventActions.fetchCategories()).then(() => dispatch(eventActions.fetchEvents())).then(() => setIsLoaded(true))
-  }, [dispatch])
+    dispatch(eventActions.fetchCategories())
+      .then(() => dispatch(eventActions.fetchEvents()))
+      .then(() => setIsLoaded(true));
+  }, [dispatch]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -51,108 +56,119 @@ const EditEventPage = () => {
       history.push(`/events/${event.id}`);
       return;
     }
-
   };
   if (isLoaded) {
-  return (
-    <div className="createEventPage">
-      <form className="createEventForm" onSubmit={handleSubmit}>
-        <div className="basicInfo">
-          <h1>Basic Info</h1>
-          <p>
-            Name your event and tell event-goers why they should come. Add
-            details that highlight what makes it unique.
-          </p>
-        </div>
-        <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
-        <div className="eventNameInput formInput">
-          <label className="eventcreatelabel">Event Title</label>
-          <input
-            onChange={(e) => setName(e.target.value)}
-            value={name}
-            required
-            placeholder="Be descriptive and clear"
-          ></input>
-        </div>
-        <div className="eventDescriptionInput formInput">
-          <label className="eventcreatelabel">Event description</label>
-          <input
-            onChange={(e) => setDescription(e.target.value)}
-            value={description}
-            required
-            placeholder="Tell people what your event is all about!"
-          ></input>
-        </div>
-        <div className="categorySelection formInput">
-          <label className="eventcreatelabel">Category</label>
-          <select
-            onChange={(e) => setCategory(e.target.value)}
-            value={category}
-          >
-            <option defaultValue hidden>
-              {singleCategory.name}
-            </option>
-            {categories.map(({ id, name }) => (
-              <option key={id} value={id}>
-                {name}
-              </option>
+    return (
+      <div className="createEventPage">
+        <form className="createEventForm" onSubmit={handleSubmit}>
+          <div className="basicInfo">
+            <h1>Basic Info</h1>
+            <p>
+              Name your event and tell event-goers why they should come. Add
+              details that highlight what makes it unique.
+            </p>
+          </div>
+          <ul>
+            {errors.map((error, idx) => (
+              <li key={idx}>{error}</li>
             ))}
-          </select>
-        </div>
-        <div className="locationInfo">
-          <h1>Location</h1>
-          <p>
-            Help people in the area discover your event and let attendees know
-            where to show up.
-          </p>
-        </div>
-        <div className="locationInput formInput">
-          <label className="eventcreatelabel">Location</label>
-          <input
-            type="address"
-            onChange={(e) => setLocation(e.target.value)}
-            value={location}
-            required
-          ></input>
-        </div>
-        <div className="dateInfo">
-          <h1>Date</h1>
-          <p>
-            Tell event-goers when your event starts so they can make plans to
-            attend.
-          </p>
-        </div>
-        <div className="formInput">
-          <label className="eventcreatelabel">Date</label>
-          <input
-            type="datetime-local"
-            onChange={(e) => setDate(e.target.value)}
-            value={date}
-            required
-            className="dateInput"
-          ></input>
-        </div>
-        <div className="capacityInfo">
-          <h1>Capacity</h1>
-          <p>How many tickets will be available for this event.</p>
-        </div>
-        <div className="formInput">
-          <label className="eventcreatelabel">Capacity</label>
-          <input
-            type="numeric"
-            onChange={(e) => setCapacity(e.target.value)}
-            value={capacity}
-            required
-          ></input>
-        </div>
-        <button type="submit">Create Event</button>
-      </form>
-    </div>
-  );} else return null
+          </ul>
+          <div className="eventNameInput formInput">
+            <label className="eventcreatelabel">Event Title</label>
+            <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              required
+              placeholder="Be descriptive and clear"
+            ></input>
+          </div>
+          <div className="eventDescriptionInput formInput">
+            <label className="eventcreatelabel">Event description</label>
+            <input
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
+              required
+              placeholder="Tell people what your event is all about!"
+            ></input>
+          </div>
+          <div className="categorySelection formInput">
+            <label className="eventcreatelabel">Category</label>
+            <select
+              onChange={(e) => setCategory(e.target.value)}
+              value={category}
+            >
+              <option defaultValue hidden>
+                {singleCategory.name}
+              </option>
+              {categories.map(({ id, name }) => (
+                <option key={id} value={id}>
+                  {name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="eventDescriptionInput formInput">
+            <label className="eventcreatelabel">Event Image</label>
+            <input
+              onChange={(e) => setImg(e.target.value)}
+              value={img}
+              required
+              placeholder="Enter your event image url here"
+            ></input>
+          </div>
+          <div className="locationInfo">
+            <h1>Location</h1>
+            <p>
+              Help people in the area discover your event and let attendees know
+              where to show up.
+            </p>
+          </div>
+          <div className="locationInput formInput">
+            <label className="eventcreatelabel">Location</label>
+            <input
+              type="address"
+              onChange={(e) => setLocation(e.target.value)}
+              value={location}
+              required
+            ></input>
+          </div>
+          <div className="dateInfo">
+            <h1>Date</h1>
+            <p>
+              Tell event-goers when your event starts so they can make plans to
+              attend.
+            </p>
+          </div>
+          <div className="formInput">
+            <label className="eventcreatelabel">Date</label>
+            <input
+              type="datetime-local"
+              onChange={(e) => setDate(e.target.value)}
+              value={date}
+              required
+              className="dateInput"
+            ></input>
+          </div>
+          <div className="capacityInfo">
+            <h1>Capacity</h1>
+            <p>How many tickets will be available for this event.</p>
+          </div>
+          <div className="formInput">
+            <label className="eventcreatelabel">Capacity</label>
+            <input
+              type="numeric"
+              onChange={(e) => setCapacity(e.target.value)}
+              value={capacity}
+              required
+            ></input>
+          </div>
+          <div>
+            <button type="submit" className="createBtn">Edit Event</button>
+          </div>
+        </form>
+      </div>
+    );
+  } else return null;
 };
 
 export default EditEventPage;
